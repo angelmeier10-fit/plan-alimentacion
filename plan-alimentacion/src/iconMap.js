297a10a26fb53d82388dbf9d193a254138b88dad
Angel Meier <angelmeier10@gmail.com>
2026-08-07 22:26:43 -0300

    Agregar ícono BookOpen a iconMap

--- plan-alimentacion/src/iconMap.js
+++ plan-alimentacion/src/iconMap.js
@@ -1,13 +1,13 @@
 import {
   Sunrise, Soup, Coffee, Moon, Carrot, Flame, Dumbbell,
   ShoppingBasket, Sparkles, ChevronRight, Beef, Fish, Egg,
-  Droplet, Candy, Info, ArrowLeftRight, Leaf,
+  Droplet, Candy, Info, ArrowLeftRight, Leaf, BookOpen,
 } from "lucide-react";
 
 const ICONS = {
   Sunrise, Soup, Coffee, Moon, Carrot, Flame, Dumbbell,
   ShoppingBasket, Sparkles, ChevronRight, Beef, Fish, Egg,
-  Droplet, Candy, Info, ArrowLeftRight, Leaf,
+  Droplet, Candy, Info, ArrowLeftRight, Leaf, BookOpen,
 };
 
 export const MEAL_ICON = { Desayuno: Sunrise, Almuerzo: Soup, Merienda: Coffee, Cena: Moon };
